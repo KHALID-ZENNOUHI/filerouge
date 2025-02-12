03bf611951be7000628aae50cfd7fe8f8bc402ae
khalid.zennouhi <144844220+KHALID-ZENNOUHI@users.noreply.github.com>
2025-02-12 11:13:19 +0100

    first commit

---
+++ src/main/java/org/dev/filerouge/FilerougeApplication.java
@@ -0,0 +1,13 @@
+package org.dev.filerouge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilerougeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilerougeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dev/filerouge/FilerougeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.filerouge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilerougeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
