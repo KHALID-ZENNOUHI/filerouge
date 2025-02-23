22ab1359b628110fbdb18f616f742fb38b9bcda5
zennouhi <khalid.zennouhi@enova.ma>
2025-02-23 15:57:22 +0100

    Add Student entity to the domain model
    Introduce the `Student` entity class extending `User`, with relationships to `Class`, `Parent`, `Absence`, and `Grade`. This sets up the foundational structure for handling student data in the system.

---
+++ src/main/java/org/dev/filerouge/domain/Student.java
@@ -0,0 +1,24 @@
+package org.dev.filerouge.domain;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Student extends User {
+    @ManyToOne
+    private Class studentClass;
+
+    @OneToOne
+    private Parent parent;
+
+    @OneToMany(mappedBy = "student")
+    private Set<Absence> absences;
+
+    @OneToMany(mappedBy = "student")
+    private Set<Grade> grades;
+}
