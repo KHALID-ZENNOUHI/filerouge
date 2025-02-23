8542c41721ea59425a1aea6c4059c5db8996a7cf
zennouhi <khalid.zennouhi@enova.ma>
2025-02-23 15:57:02 +0100

    Add Administrator entity extending User
    Introduced a new Administrator entity in the domain layer, extending the User class. This includes JPA annotations for persistence and Lombok annotations for boilerplate code reduction.

---
+++ src/main/java/org/dev/filerouge/domain/Administrator.java
@@ -0,0 +1,13 @@
+package org.dev.filerouge.domain;
+
+import lombok.*;
+import jakarta.persistence.*;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Administrator extends User {
+
+}
