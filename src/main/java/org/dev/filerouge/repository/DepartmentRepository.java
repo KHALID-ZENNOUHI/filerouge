ae7bfa70a7a43ea18eaf460d4c0bb34f8f9be839
zennouhi <khalid.zennouhi@enova.ma>
2025-02-23 16:04:32 +0100

    Add DepartmentRepository interface
    Introduce a repository interface for managing Department entities using JpaRepository. This enables standard CRUD operations and integration with the persistence layer.

---
+++ src/main/java/org/dev/filerouge/repository/DepartmentRepository.java
@@ -0,0 +1,10 @@
+package org.dev.filerouge.repository;
+
+import org.dev.filerouge.domain.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.UUID;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, UUID> {
+}
