060f08ef845aa1d777dbce40bc326d66d3578a04
zennouhi <khalid.zennouhi@enova.ma>
2025-02-23 16:04:15 +0100

    Add ActivityRepository interface
    Introduce ActivityRepository to manage Activity entities using JPA. This provides CRUD operations and integration with the database, leveraging UUID as the entity identifier.

---
+++ src/main/java/org/dev/filerouge/repository/ActivityRepository.java
@@ -0,0 +1,11 @@
+package org.dev.filerouge.repository;
+
+import org.dev.filerouge.domain.Activity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ActivityRepository extends JpaRepository<Activity, UUID> {
+}
