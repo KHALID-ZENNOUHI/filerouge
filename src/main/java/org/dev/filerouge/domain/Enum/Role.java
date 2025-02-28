d4ef761a9345692f6163cbd96fb21bc94b170307
zennouhi <khalid.zennouhi@enova.ma>
2025-02-28 11:56:13 +0100

    Refactor Role enum to include permissions per role.
    Updated the Role enum to define specific permissions for each role. Introduced a Set of permissions and a getter method to access them, improving role-based access control structure.

--- src/main/java/org/dev/filerouge/domain/Enum/Role.java
+++ src/main/java/org/dev/filerouge/domain/Enum/Role.java
@@ -1,9 +1,20 @@
 package org.dev.filerouge.domain.Enum;
 
+import java.util.Set;
 public enum Role {
-    STUDENT,
-    TEACHER,
-    PARENT,
-    ADMINISTRATOR
+    ROLE_STUDENT(Set.of("CAN_VIEW_GRADE", "CAN_VIEW_ABSENT")),
+    ROLE_TEACHER(Set.of("CAN_ADD_GRADE", "CAN_VIEW_GRADE", "CAN_ADD_ABSENT", "CAN_VIEW_ABSENT")),
+    ROLE_PARENT(Set.of("CAN_VIEW_GRADE", "CAN_VIEW_ABSENT")),
+    ROLE_ADMINISTRATOR(Set.of("CAN_ADD_GRADE", "CAN_VIEW_GRADE", "CAN_ADD_ABSENT", "CAN_VIEW_ABSENT", "CAN_ADD_TEACHER", "CAN_VIEW_TEACHER","CAN_ADD_STUDENT", "CAN_VIEW_STUDENT"));
+
+    private final Set<String> permissions;
+
+    Role(Set<String> permissions) {
+        this.permissions = permissions;
+    }
+
+    public Set<String> getPermissions() {
+        return permissions;
+    }
 }
 
