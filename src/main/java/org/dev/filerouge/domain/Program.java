cb2dd89a0b7e1dc04ee55496c8c446c873c0427d
zennouhi <khalid.zennouhi@enova.ma>
2025-02-28 11:57:57 +0100

    Add Program entity to domain model
    Introduce the Program entity with JPA annotations, including associations with Class and Subject. This entity captures program details such as description and is identified by a UUID primary key.

---
+++ src/main/java/org/dev/filerouge/domain/Program.java
@@ -0,0 +1,26 @@
+package org.dev.filerouge.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Program {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @ManyToOne
+    private Class Classes;
+
+    @ManyToOne
+    private Subject subject;
+
+    private String description;
+}
